043c6f9b81a0ca86f8d3342bda49c48d8d0b9df2
Hugo Costilla <hugo.costillah@gmail.com>
2019-06-26 19:11:42 -0500

    Menu Footer Terminado

--- config/js/events/menu.js
+++ config/js/events/menu.js
@@ -1,4 +1,9 @@
 export default function menu(){
+    sideMenu();
+    footerMenu();
+}
+
+function sideMenu(){
     var menu = document.getElementsByClassName("menu");
     var slide = document.getElementById("slide_m");
     var close = document.getElementById("m_close");
@@ -13,5 +18,24 @@ export default function menu(){
 
     function s_close(){
         slide.style.width = "0";
+    }
+}
+
+function footerMenu(){
+    var f_menu = document.getElementById("footerMenu");
+    var choose = document.getElementById("choose");
+    var r_footer = document.getElementById("r_footer");
+    var r_close = document.getElementById("f_close");
+
+    choose.addEventListener("click", footer);
+
+    r_close.addEventListener("click", ft_close);
+
+    function footer(){
+        f_menu.style.height = "45vh";
+    }
+
+    function ft_close(){
+        f_menu.style.height = "0";
     }
 }
