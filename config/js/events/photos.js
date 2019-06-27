6bf3268246e0f412b9c2053ac26ba7536e9b6d90
Hugo Costilla <hugo.costillah@gmail.com>
2019-06-26 20:16:04 -0500

    Cambio de Galeria (interior y exterior)

--- config/js/events/photos.js
+++ config/js/events/photos.js
@@ -1,4 +1,9 @@
 export default function photos(){
+    gallery();
+    btn_change();
+}
+
+function gallery(){
     var image = document.getElementsByClassName("zoom");
     var m_image = document.getElementById("m_image");
     var modal = document.getElementById("modal");
@@ -17,6 +22,33 @@ export default function photos(){
 
     function close(){
         modal.style.display = "none";
+    }
+}
+
+function btn_change(){
+    var btn_out = document.getElementById("out");
+    var btn_in = document.getElementById("in");
+    var inside = document.getElementById("inside");
+    var outside = document.getElementById("outside");
+
+    btn_out.addEventListener("click", showOut);
+
+    function showOut(){
+        btn_out.style.borderBottom = "1vh solid red";
+        btn_in.style.borderBottom = "1vh solid white";
+
+        inside.style.display = "none";
+        outside.style.display = "flex";
+    }
+
+    btn_in.addEventListener("click", showIn);
+
+    function showIn(){
+        btn_in.style.borderBottom = "1vh solid red";
+        btn_out.style.borderBottom = "1vh solid white";
+
+        outside.style.display = "none";
+        inside.style.display = "flex";
     }
 
 }
