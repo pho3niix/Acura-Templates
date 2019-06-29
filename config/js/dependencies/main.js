c30d70999933136fad0f795c9d80bbc835a6a235
Hugo Costilla <hugo.costillah@gmail.com>
2019-06-29 16:45:59 -0500

    Imagen 360 version optimizada

--- config/js/dependencies/main.js
+++ config/js/dependencies/main.js
@@ -1,3 +1,5 @@
+// Inicializar las funciones del sitio
+
 window.onload = function(){
     range();
     brochure();
@@ -6,98 +8,142 @@ window.onload = function(){
     footerMenu();
 }
 
+// Funcion 360 para imagen detalle auto
+
 function range(){
     var btn_360 = document.getElementById("p_360");
     var range = document.getElementById("range");
     var close = document.getElementById("close_r");
     var image = document.getElementById("car_360");
 
+    // Variables de ruta e imagen estatica
+    var ruta = "../images/cars/";
+    var i_static = ruta + "camioneta.png";
+
+    // Variables de imagenes 360
+    var _1 = ruta + "02 mdx exterior 01.png";
+    var _2 = ruta + "02 mdx exterior 02.png";
+    var _3 = ruta + "02 mdx exterior 03.png";
+    var _4 = ruta + "02 mdx exterior 04.png";
+    var _5 = ruta + "02 mdx exterior 05.png";
+    var _6 = ruta + "02 mdx exterior 06.png";
+    var _7 = ruta + "02 mdx exterior 07.png";
+    var _8 = ruta + "02 mdx exterior 08.png";
+    var _9 = ruta + "02 mdx exterior 09.png";
+    var _10 = ruta + "02 mdx exterior 10.png";
+    var _11 = ruta + "02 mdx exterior 11.png";
+    var _12 = ruta + "02 mdx exterior 12.png";
+    var _13 = ruta + "02 mdx exterior 13.png";
+    var _14 = ruta + "02 mdx exterior 14.png";
+    var _15 = ruta + "02 mdx exterior 15.png";
+    var _16 = ruta + "02 mdx exterior 16.png";
+    var _17 = ruta + "02 mdx exterior 17.png";
+    var _18 = ruta + "02 mdx exterior 18.png";
+    var _19 = ruta + "02 mdx exterior 19.png";
+    var _20 = ruta + "02 mdx exterior 20.png";
+    var _21 = ruta + "02 mdx exterior 21.png";
+    var _22 = ruta + "02 mdx exterior 22.png";
+    var _23 = ruta + "02 mdx exterior 23.png";
+    var _24 = ruta + "02 mdx exterior 24.png";
+
+    // Condicion para cambiar imagenes del 360
     range.addEventListener("input", function(){
-        if(range.value == 1){
-            image.src = "../images/cars/02 mdx exterior 01.png";
-        } else if(range.value == 2){
-            image.src = "../images/cars/02 mdx exterior 02.png";
-        }else if(range.value == 3){
-            image.src = "../images/cars/02 mdx exterior 03.png";
-        }else if(range.value == 4){
-            image.src = "../images/cars/02 mdx exterior 04.png";}
-        else if(range.value == 5){
-            image.src = "../images/cars/02 mdx exterior 05.png";
-        }
-        else if(range.value == 6){
-            image.src = "../images/cars/02 mdx exterior 06.png";
-        }
-        else if(range.value == 7){
-            image.src = "../images/cars/02 mdx exterior 07.png";
-        }
-        else if(range.value == 8){
-            image.src = "../images/cars/02 mdx exterior 08.png";
-        }
-        else if(range.value == 9){
-            image.src = "../images/cars/02 mdx exterior 09.png";
-        }
-        else if(range.value == 10){
-            image.src = "../images/cars/02 mdx exterior 10.png";
-        }
-        else if(range.value == 11){
-            image.src = "../images/cars/02 mdx exterior 11.png";
-        }
-        else if(range.value == 12){
-            image.src = "../images/cars/02 mdx exterior 12.png";
-        }
-        else if(range.value == 13){
-            image.src = "../images/cars/02 mdx exterior 13.png";
-        }
-        else if(range.value == 14){
-            image.src = "../images/cars/02 mdx exterior 14.png";
-        }
-        else if(range.value == 15){
-            image.src = "../images/cars/02 mdx exterior 15.png";
-        }
-        else if(range.value == 16){
-            image.src = "../images/cars/02 mdx exterior 16.png";
-        }
-        else if(range.value == 17){
-            image.src = "../images/cars/02 mdx exterior 17.png";
-        }
-        else if(range.value == 18){
-            image.src = "../images/cars/02 mdx exterior 18.png";
-        }
-        else if(range.value == 19){
-            image.src = "../images/cars/02 mdx exterior 19.png";
-        }
-        else if(range.value == 20){
-            image.src = "../images/cars/02 mdx exterior 20.png";
-        }
-        else if(range.value == 21){
-            image.src = "../images/cars/02 mdx exterior 21.png";
-        }
-        else if(range.value == 22){
-            image.src = "../images/cars/02 mdx exterior 22.png";
-        }
-        else if(range.value == 23){
-            image.src = "../images/cars/02 mdx exterior 23.png";
-        }else{
-            image.src = "../images/cars/02 mdx exterior 24.png";
-        }
+       if(range.value>=1 && range.value<=12){
+           if(range.value<=6){
+               if(range.value<=3){
+                   if(range.value == 1){
+                       image.src = _1;
+                   }else if(range.value == 2){
+                       image.src = _2;
+                   }else{
+                       image.src = _3;
+                   }
+               }else{
+                   if(range.value == 4){
+                       image.src = _4;
+                   }else if(range.value == 5){
+                       image.src = _5;
+                   }else{
+                       image.src = _6;
+                   }
+               }
+           }else if(range.value>=7 && range.value<=12){
+                if(range.value<=9){
+                    if(range.value == 7){
+                        image.src = _7;
+                    }else if(range.value == 8){
+                        image.src = _8;
+                    }else{
+                        image.src = _9;
+                    }
+                }else{
+                    if(range.value == 10){
+                        image.src = _10;
+                    }else if(range.value == 11){
+                        image.src = _11;
+                    }else{
+                        image.src = _12;
+                    }
+                }
+           }
+       }else if(range.value>=13 && range.value<=18){
+            if(range.value<=15){
+                if(range.value == 13){
+                    image.src = _13;
+                }else if(range.value == 14){
+                    image.src = _14;
+                }else{
+                    image.src = _15;
+                }
+            }else{
+                if(range.value == 16){
+                    image.src = _16;
+                }else if(range.value == 17){
+                    image.src = _17;
+                }else{
+                    image.src = _18;
+                }
+            }
+       }else if(range.value>=19 && range.value<=24){
+            if(range.value<=21){
+                if(range.value == 19){
+                    image.src = _19;
+                }else if(range.value == 20){
+                    image.src = _20;
+                }else{
+                    image.src = _21;
+                }
+            }else{
+                if(range.value == 22){
+                    image.src = _22;
+                }else if(range.value == 23){
+                    image.src = _23;
+                }else{
+                    image.src = _24;
+                }
+            }
+       }
     })
 
+    // Habilitar funcion 360
     btn_360.addEventListener("click", function(){
         btn_360.style.display = "none";
         range.style.display = "block";
         close.style.display = "block";
-        image.src = "../images/cars/02 mdx exterior 01.png";
+        image.src = _1;
     });
 
+    // Deshabilitar funcion 360
     close.addEventListener("click", function(){
         btn_360.style.display = "block";
         range.style.display = "none";
         close.style.display = "none";
         range.value = 1;
-        image.src = "../images/cars/camioneta.png";
+        image.src = i_static;
     });
 }
 
+// Modal brochure
 function brochure(){
     var modal = document.getElementById("modal");
     var close_m = document.getElementById("x_close");
@@ -113,6 +159,7 @@ function brochure(){
     });
 }
 
+// Modal cotizar
 function cotizar(){
     var form = document.getElementById("modal2");
     var close_f = document.getElementById("ft_close");
@@ -128,6 +175,7 @@ function cotizar(){
     });
 }
 
+// Menu lateral
 function sideMenu(){
     var menu = document.getElementsByClassName("menu");
     var slide = document.getElementById("slide_m");
@@ -146,6 +194,7 @@ function sideMenu(){
     }
 }
 
+// Footer menu slide
 function footerMenu(){
     var f_menu = document.getElementById("footerMenu");
     var choose = document.getElementById("choose");
