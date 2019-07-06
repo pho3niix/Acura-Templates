2ea8ab2184c50dd7bd8611d64f440058c045754b
Hugo Costilla <hugo.costillah@gmail.com>
2019-07-06 03:10:57 -0500

    Animaciones en archivos separados

---
+++ src/js/dependencies/animations.js
@@ -0,0 +1,52 @@
+window.onload = function(){
+    var move1 = document.getElementById("btns_right");
+    var move2 = document.getElementById("btns_left");
+    var r_square = document.getElementById("r_square");
+    var b_square = document.getElementById("b_square");
+    var clicks = 0;
+
+    function play(){
+        sp.next();
+        sps.next();
+        spx.next();
+
+        clicks +=1;
+
+        r_square.setAttribute("class", "move"+clicks);
+        b_square.setAttribute("class", "moves"+clicks);
+
+        if(clicks==4){
+            r_square.removeAttribute("class", "move");
+            clicks = 0;
+        }
+    }
+
+    var autoplay = setInterval(function(){
+        play();
+    },5000);
+
+    move1.addEventListener("click", function(){
+        clicks +=1;
+
+        r_square.setAttribute("class", "move"+clicks);
+        b_square.setAttribute("class", "moves"+clicks);
+
+        if(clicks==4){
+            r_square.removeAttribute("class", "move");
+            clicks = 0;
+        }
+
+    });
+
+    move2.addEventListener("click", function(){
+        clicks -=1;
+
+        r_square.setAttribute("class", "move"+clicks);
+        b_square.setAttribute("class", "moves"+clicks);
+
+        if(clicks == -4){
+            r_square.removeAttribute("class", "move");
+            clicks = 0;
+        }
+    });
+}
