2c1a9bf9e45c39b324971a5aae049856b3141880
Hugo Costilla <hugo.costillah@gmail.com>
2019-07-09 02:11:18 -0500

    Especificaciones cambiar interior y exterior

--- src/js/dependencies/tec_modal.js
+++ src/js/dependencies/tec_modal.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
     mdl_image();
+    changeBack();
 });
 
 function mdl_image(){
     var show = document.getElementById("shows");
     var mdlPre = document.getElementById("pre_modal");
     var close = document.getElementById("e_close");
+    var show2 = document.getElementById("shows2");
+    var mdlPre2 = document.getElementById("pre_modal2");
+    var close2 = document.getElementById("e_close2");
 
     show.addEventListener("click", function(){
         mdlPre.style.display = "flex";
@@ -14,7 +18,43 @@ function mdl_image(){
 
     });
 
+    show2.addEventListener("click", function(){
+        mdlPre2.style.display = "flex";
+        mdlPre2.style.animationName = "zoom";
+        mdlPre2.style.animationDuration = ".5s";
+
+    });
+
     close.addEventListener("click", function(){
         mdlPre.style.display = "none";
     });
+
+    close2.addEventListener("click", function(){
+        mdlPre2.style.display = "none";
+    });
+}
+
+
+function changeBack(){
+    var interior = document.getElementById("interior");
+    var exterior = document.getElementById("exterior");
+    var btnInt = document.getElementById("link_int");
+    var btnOut = document.getElementById("link_out");
+    var mdlPre = document.getElementById("pre_modal");
+    var mdlPre2 = document.getElementById("pre_modal2");
+
+    btnInt.addEventListener("click", function(){
+        interior.style.display = "block";
+        exterior.style.display = "none";
+        interior.style.animationName = "zoom";
+        interior.style.animationDuration = ".5s";
+        mdlPre2.style.display = "none";
+    });
+
+    btnOut.addEventListener("click", function(){
+        interior.style.display = "none";
+        exterior.style.display = "block";
+        mdlPre.style.display = "none";
+    });
+
 }
